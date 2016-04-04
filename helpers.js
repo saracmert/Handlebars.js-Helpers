b576ee38f20bff5c1fab4e8c36b8e927567777ab
Mert Sarac <me@mertsarac.com>
2016-04-05 00:53:10 +0300

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,33 @@
+Handlebars.registerHelper("convertMoment", function (datetime) {
+    if (moment) {
+        return moment(new Date(parseInt(datetime))).fromNow();
+    }
+    else {
+        return datetime;
+    }
+});
+
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
